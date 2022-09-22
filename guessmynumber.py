9b41c158a96841b50a7eef629c3bdae80ca01fef
dominonb <janlucas.sternberg@gmail.com>
2022-09-22 11:53:51 +0200

    22.9

---
+++ guessmynumber.py
@@ -0,0 +1,25 @@
+print("Please think of a number between 0 and 100!")
+low = 0
+high = 100
+i = 0
+while i in range(0,100):
+	guess = int((low + high)/2)
+	print("Is your secret number" + " " + str(guess) + "?")
+	response = str(input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guess correctly."))
+	if response == 'h':
+		#0,100 => 5
+		high = guess
+		guess = int((low+high)/2)
+		
+	elif response == 'l':
+		#0,100 => 90
+		low = guess
+		guess = int((low+high)/2)
+
+	elif response == 'c':
+		#print("You guessed it correctly!")
+		break
+
+	else:
+		print("Enter only c or h or l")
+print("Game over. Your secret number was:" + " " + str(guess))
---
+++ odd.py
@@ -0,0 +1,10 @@
+def odd(x):
+    '''
+    x: int
+    returns: True if x is odd, False otherwise
+    '''
+    while x % 2 != 0:
+        return True
+        break
+    
+    return False
