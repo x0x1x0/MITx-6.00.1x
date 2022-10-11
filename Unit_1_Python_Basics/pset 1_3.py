504c8a7c9f456da9e4cd9b6a14db3f620fe48395
dominonb <janlucas.sternberg@gmail.com>
2022-10-11 23:33:13 +0200

    comprehensive doc for pset 1 3

--- Unit_1_Python_Basics/pset 1_3.py
+++ Unit_1_Python_Basics/pset 1_3.py
@@ -11,22 +11,37 @@ Longest substring in alphabetical order is: abc
 """
 
 # letters can be compared by < > == etc.
+# the 0 index of s is the first letter. 
+# Therefore we start with a string at s[0]
+# for every index in s we check wether the letter at the indexed position
+# is alphabetically "greater or equal" to the previous.
+
+# if that is the case we add that letter to our temporary string
+# now, if our tempstring is longer than the longest string recorded so far
+# we set our longest_yet to be the length of the tempstring
+# and let our tempstring sit in place of the longest_total
+# which will sit in place until a new longer tempstring takes its place.
+# we now increase our index by one, to move to the next letter.
+
+# if our letter is not greater or equal to the previous
+# we simply make the tempstring be the next letter up and start again
+# this also resets the tempstring if the following letter
+# is not in alphabetical order.
 
 s = "hellygreiftdenkederan"
-    
-maxlength = 0
-current = s[0]
-longest = s[0]
+   
+longest_yet = 0 
+tempstring = s[0]
+longest_total = s[0]
 
-for i in range(len(s) - 1):
+for i in range(len(s)-1):
 	if s[i+1] >= s[i]:
-		current += s[i+1]
-		if len(current) > maxlength:
-			maxlength = len(current)
-			longest = current
+		tempstring += s[i+1]
+		if len(tempstring) > longest_yet:
+			longest_yet = len(tempstring)
+			longest_total = tempstring
 	else:
-		current = s[i+1]
+		tempstring = s[i+1]
 	i += 1
-
-print(current)
-print("The longest substring in alphabetical order is:" + longest)
+ 
+print("The longest substring in alphabetical order is: " + longest_total)
